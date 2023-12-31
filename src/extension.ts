398ea24091b18d1b2cb99557193db5a5e1044f0d
ly2011 <1518550424@qq.com>
2023-12-31 11:44:16 +0800

    feat: vscode接入webview挂载ocr识别(react)页面

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import path from 'node:path';
+
+const cats = {
+  'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+  'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif'
+};
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,6 +23,47 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from vscode-lowcode!');
+
+    // Create and show a new webview
+    const panel = vscode.window.createWebviewPanel(
+      'OCR',
+      'OCR 识别',
+      vscode.ViewColumn.One,
+      {
+        retainContextWhenHidden: true, // 保证 Webview 所在页面进入后台时不被释放
+        enableScripts: true, // 运行 JS 执行
+      }
+    );
+
+    const isProduction = context.extensionMode === vscode.ExtensionMode.Production;
+    let srcUrl = '';
+    if (isProduction) {
+      // const filePath = vscode.Uri.file(
+      //   path.join(context.extensionPath, 'dist', 'static/js/main.js')
+      // );
+      // srcUrl = panel.webview.asWebviewUri(filePath).toString();
+      // 生产环境待完善
+      srcUrl = 'http://localhost:5173';
+    } else {
+      srcUrl = 'http://localhost:5173';
+    }
+
+    panel.webview.html = getWebviewContent(srcUrl);
+
+    // const updateWebview = () => {
+    //   panel.webview.html = getWebviewContent(srcUrl);
+    // };
+
+    // updateWebview();
+    // const interval = setInterval(updateWebview, 1000);
+
+    // panel.onDidDispose(
+    //   () => {
+    //     clearInterval(interval);
+    //   },
+    //   null,
+    //   context.subscriptions,
+    // );
 	});
 
 	context.subscriptions.push(disposable);
@@ -24,3 +71,42 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent(srcUri: string) {
+  return `<!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>OCR识别</title>
+      <style>
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            background-color:#000000;
+            overflow:hidden;
+        }
+        .webView_iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+        }
+        .outer{
+          width: 100%;
+          height: 100%;
+          overflow: hidden;
+        }
+      </style>
+    </head>
+    <body>
+    <div class="outer">
+      <iframe id='WebviewIframe' class="webView_iframe" sandbox="allow-scripts allow-same-origin allow-forms allow-pointer-lock allow-downloads" allow="cross-origin-isolated; clipboard-read; clipboard-write;" src="http://localhost:5173/"></iframe>
+    </div>
+    </body>
+  </html>
+  `;
+}
--- src/web/components/OcrUploader.tsx
+++ src/web/components/OcrUploader.tsx
@@ -32,11 +32,11 @@ const OcrUploader: React.FC<IOcrUploaderProps> = () => {
     setText("");
 
     // 1. 识别简体中文+繁体中文+英文("eng+chi_sim+chi_tra")，但是目前识别简体中文很多识别不准
-    // 2. 设置离线加载本地语言包及tesseract库
+    // 2.  TODO: 设置离线加载本地语言包及tesseract库，在vscode插件中会报错
     const worker = await createWorker("eng+chi_sim", 1, {
-      workerPath: "/tesseract/tesseract.js/worker.min.js",
-      langPath: "/tesseract/tesseract.js/lang-data",
-      corePath: "/tesseract/tesseract.js-core",
+      // workerPath: "/tesseract/tesseract.js/worker.min.js",
+      // langPath: "/tesseract/tesseract.js/lang-data",
+      // corePath: "/tesseract/tesseract.js-core",
       logger: (m) => console.log(m),
     });
 
